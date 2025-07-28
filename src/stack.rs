fb70b1b13a564162bf4fde8d2cca7ea56de53bc3
siyasjr <siyasjr@gmail.com>
2025-07-28 23:51:20 +0530

    This code shows how Rust uses the stack for most primitive data types whoose size is known at compile time

---
+++ src/stack.rs
@@ -0,0 +1,13 @@
+fn main() {
+    stack_fn();   // Call the function that uses stack memory
+  
+}
+
+fn stack_fn() {
+    // Declare a few integers on the stack
+    let a = 10;
+    let b = 20;
+    let c = a + b;
+    println!("Stack function: The sum of {} and {} is {}", a, b, c);
+}
+
