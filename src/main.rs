c9f999c3d9647ba32745196a27cc964d1c08849c
siyasjr <siyasjr@gmail.com>
2025-07-29 23:44:53 +0530

    ownerships

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
 fn main() {
-    print!("Hello World");
-  
+    let mut s1 = String::from("hello");
+    s1 = takes_ownership(s1);
+    println!("{}", s1);
+}
+
+fn takes_ownership(some_string: String) -> String {
+    println!("{}", some_string); 
+    return some_string; // return the string ownership back to the original main fn
 }
---
+++ src/ownership.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let my_string = String::from("hello");
+    takes_ownership(my_string);
+    println!("{}", my_string); // This line would cause a compile error because ownership has been moved.
+}
+
+fn takes_ownership(some_string: String) {
+    println!("{}", some_string); // `some_string` now owns the data.
+}
