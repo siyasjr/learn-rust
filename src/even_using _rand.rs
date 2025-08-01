e8496d6fd788fc1f945eb0623740dc16fc706ce7
siyasjr <siyasjr@gmail.com>
2025-08-01 23:57:21 +0530

    just formatted

--- src/even_using _rand.rs
+++ src/even_using _rand.rs
@@ -1,24 +1,21 @@
+use rand::{Rng, rng};
 #[derive(Debug)]
-use rand::Rng;
 
-
-enum Oddoreven {
-        Odd,
-        Even,
-    }
-
-
-fn main(){
-    let mut rng = rng(); // renamed from thread_rng()
-    let num: i32 = rng.random_range(0..1000); // renamed from gen_range()
-    println!("{} is {:?}", num,is_even(num) );
+enum OddorEven {
+    Odd, 
+    Even,
 }
 
-fn is_even(num: i32) -> Oddoreven {
-
-    if num % 2 == 0 {
-        return Oddoreven::Even;
-        
-    }
-    return Oddoreven::Odd
+fn main (){
+    let mut rng = rng();
+    let num: i32 = rng.random_range(0..5000);
+    println!("{} is {:?}", num, is_even(num) );
 }
+
+fn is_even(num: i32) -> OddorEven{
+    if num % 2 ==0 {
+        return OddorEven::Even;
+    }
+    return OddorEven::Odd
+ }
+
