f81b5b138f634dc180a689687c54af86942214ef
siyasjr <siyasjr@gmail.com>
2025-08-02 03:20:35 +0530

    simple interactive cli  adder using std::io

---
+++ add_nums_intrctv_cli.rs
@@ -0,0 +1,22 @@
+//simple interactive cli  adder using std::io
+
+use std::io;
+
+fn main(){
+    let mut input: String = String::from("");
+    println!("Enter the first number");
+
+
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    let a: i32 = input.trim().parse().expect("Enter a valid number");
+
+    input.clear();
+
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    let b:i32 = input.trim().parse().expect("Enter a valid number");
+
+    let result = a + b ;
+    println!("{} + {} = {}", a,b,result);
+
+
+}
