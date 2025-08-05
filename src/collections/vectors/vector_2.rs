c67f71043afe6bdff4cb8e8fee94c7ad10919fdd
siyasjr <siyasjr@gmail.com>
2025-08-05 18:07:26 +0530

    Add basic vector push and print example

---
+++ src/collections/vectors/vector_2.rs
@@ -0,0 +1,28 @@
+// Adds elements to a vector, prints each element, and then prints the full vector.
+
+fn main() {
+
+    let mut vec = vec![1,2,3];
+    vec.push(4);
+    vec.push(5);
+
+
+    for val in &vec{
+    println!("{}", val)
+}
+    println!("{:?}", vec);
+
+}fn main() {
+
+    let mut vec = vec![1,2,3];
+    vec.push(4);
+    vec.push(5);
+
+
+    for val in &vec{
+    println!("{}", val)
+}
+    println!("{:?}", vec);
+
+}
+
