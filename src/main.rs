a618eb34405f4ec56d05266e466fea7b0c3fa482
siyasjr <siyasjr@gmail.com>
2025-08-06 21:53:20 +0530

    default

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,3 @@
     println!("This is the entry point file")
 }*/
 
-
-use std::collections::HashMap;
-
-fn main(){
-    let vec = vec![(String::from("Fayyaz"), 27), (String::from("Akmal"), 28), (String::from("Salam"), 29)];
-    let mut hm = HashMap::new();
-    for (key, val ) in vec{
-
-        hm.insert(key, val);
-
-    }
-
-    println!("Full hashMap: {:?}", hm);
-    for (key, val ) in hm{
-        println!("Key:{}, val: {}", key, val);
-    }
-
-    
-}
