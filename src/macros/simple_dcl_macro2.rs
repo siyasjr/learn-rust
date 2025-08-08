a8d59c21164c2dcb86aeded497d216008f21b7bc
siyasjr <siyasjr@gmail.com>
2025-08-08 23:59:05 +0530

    Add macro_rules! example for printing multiple variadic arguments

---
+++ src/macros/simple_dcl_macro2.rs
@@ -0,0 +1,11 @@
+/*macro_rules! django {
+    ($($arg:expr),*) => {
+        
+        $(println!("{}", $arg);)*
+    };
+}
+
+fn main() {
+    django!("ssdcsfsdfvfswfswfwsfwsfdw", "gsdfsdfs", "dssfwsewaedsw",2554);
+}
+*/
