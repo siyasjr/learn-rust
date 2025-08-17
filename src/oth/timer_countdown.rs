7f433c17c14886067904d6698574ca9b724d75a7
siyasjr <siyasjr@gmail.com>
2025-08-17 23:37:41 +0530

    Counts down from N seconds, printing each second.

---
+++ src/oth/timer_countdown.rs
@@ -0,0 +1,11 @@
+// timer.rs
+use std::{env, thread, time};
+
+fn main() {
+    let secs: u64 = env::args().nth(1).unwrap_or("5".into()).parse().unwrap();
+    for i in (1..=secs).rev() {
+        println!("{}...", i);
+        thread::sleep(time::Duration::from_secs(1));
+    }
+    println!("Time's up!");
+}
