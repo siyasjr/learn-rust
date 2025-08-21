c3addf70d9e8f0d248667c04cc6766bbdec43ed5
siyasjr <siyasjr@gmail.com>
2025-08-21 23:26:02 +0530

    read a line from stdin

---
+++ src/std::io/readline.rs
@@ -0,0 +1,8 @@
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    println!("Enter your name:");
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    println!("Hello, {}", input.trim());
+}
