5411332ab64e4184613959987d7f36b116237a3e
siyasjr <siyasjr@gmail.com>
2025-08-22 22:46:14 +0530

    Valid Parentheses

---
+++ src/oth/valid_paranthesis.rs
@@ -0,0 +1,17 @@
+// Problem: https://leetcode.com/problems/valid-parentheses/
+
+impl Solution {
+    pub fn is_valid(s: String) -> bool {
+        let mut stack = Vec::new();
+        for c in s.chars() {
+            match c {
+                '(' | '{' | '[' => stack.push(c),
+                ')' => if stack.pop() != Some('(') { return false; },
+                '}' => if stack.pop() != Some('{') { return false; },
+                ']' => if stack.pop() != Some('[') { return false; },
+                _ => (),
+            }
+        }
+        stack.is_empty()
+    }
+}
