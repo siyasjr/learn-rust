d4a3f8881c5457ea0956185ac31bbdba15e0a364
siyasjr <siyasjr@gmail.com>
2025-08-22 22:46:56 +0530

    Merge Two Sorted Lists

---
+++ src/oth/LeetCode/merge_sorted_lists.rs
@@ -0,0 +1,30 @@
+
+// Problem: https://leetcode.com/problems/merge-two-sorted-lists/
+
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl Solution {
+    pub fn merge_two_lists(
+        l1: Option<Box<ListNode>>, 
+        l2: Option<Box<ListNode>>
+    ) -> Option<Box<ListNode>> {
+        match (l1, l2) {
+            (Some(mut n1), Some(mut n2)) => {
+                if n1.val < n2.val {
+                    n1.next = Solution::merge_two_lists(n1.next, Some(n2));
+                    Some(n1)
+                } else {
+                    n2.next = Solution::merge_two_lists(Some(n1), n2.next);
+                    Some(n2)
+                }
+            }
+            (Some(n1), None) => Some(n1),
+            (None, Some(n2)) => Some(n2),
+            (None, None) => None,
+        }
+    }
+}
